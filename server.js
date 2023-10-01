2026f04699e8b32faa1c8e13117f15466b4b3834
Tobi Ade <omoniyiadejengbe@gmail.com>
2023-10-01 11:34:12 +0100

    setting up the server file

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const videoRoutes = require("./routes/video.routes")
+const errorHandler = require("./middleware")
+
+
+
+app.use('/', videoRoutes);
+
+
+
+app.listen(PORT, () => {
+  console.log(`App running on ${PORT}`)
+});
